ff3f4f8dfd92d2749bfcf543bc3310f1afe86d13
Juan Bernardo Lopez Acosta <jblopezacosta@gmail.com>
2025-05-31 11:25:17 -0600

    Update quick_sort.py

--- metodos de ordenamiento/quick_sort.py
+++ metodos de ordenamiento/quick_sort.py
@@ -1,1 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivote = arr[len(arr) // 2]
+    menores = [x for x in arr if x < pivote]
+    iguales = [x for x in arr if x == pivote]
+    mayores = [x for x in arr if x > pivote]
+    return quick_sort(menores) + iguales + quick_sort(mayores)
 
+# Ejemplo
+lista = [10, 7, 8, 9, 1, 5]
+print("Ordenado:", quick_sort(lista))
