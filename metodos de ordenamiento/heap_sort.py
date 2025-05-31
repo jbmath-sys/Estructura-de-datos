5e787edc20465c2f4c9e3452658639bfeecdf794
Juan Bernardo Lopez Acosta <jblopezacosta@gmail.com>
2025-05-31 11:25:57 -0600

    Update heap_sort.py

--- metodos de ordenamiento/heap_sort.py
+++ metodos de ordenamiento/heap_sort.py
@@ -1,1 +1,26 @@
+def heapify(arr, n, i):
+    mayor = i
+    izq = 2 * i + 1
+    der = 2 * i + 2
+
+    if izq < n and arr[izq] > arr[mayor]:
+        mayor = izq
+    if der < n and arr[der] > arr[mayor]:
+        mayor = der
+    if mayor != i:
+        arr[i], arr[mayor] = arr[mayor], arr[i]
+        heapify(arr, n, mayor)
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    return arr
+
+# Ejemplo
+lista = [4, 10, 3, 5, 1]
+print("Ordenado:", heap_sort(lista))
 
